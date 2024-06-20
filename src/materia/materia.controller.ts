13b5310a20cdea1dc5e12e5f3807aa72c55937db
Leonardo <leowitcroz@gmail.com>
2024-06-20 13:49:28 -0300

    refatoração do bando de dados para foreing keys

--- src/materia/materia.controller.ts
+++ src/materia/materia.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post } from '@nestjs/common';
 import { MateriaService } from './materia.service';
 
 @Controller('materia')
@@ -6,20 +6,30 @@ export class MateriaController {
 
     constructor(private readonly materia: MateriaService) { }
 
-    @Get()
-    async read() {
-        return this.materia.readAll()
+    @Get() 
+    async read(@Body() aluno_id) {
+        return this.materia.readAll(aluno_id)
     }
 
-    @Post()
-    async create(@Body() { name, horas, faltas }) {
-        return this.materia.create({ name, horas, faltas })
+    @Post('/aluno')
+    async createAluno(@Body() { name, password }) {
+        return this.materia.createAluno({ name, password })
     }
 
-    @Delete(':id')
-    async delete(@Param('id', ParseIntPipe) id) {
-        return this.materia.delete(id);
+    @Post('/materias')
+    async createMateria(@Body() { aluno_id, nome, horas, faltas }) {
+        return this.materia.createMateira({ aluno_id, nome, horas, faltas })
     }
+
+    // @Delete(':id')
+    // async delete(@Param('id', ParseIntPipe) id) {
+    //     return this.materia.delete(id);
+    // }
+
+    // @Patch(':id') 
+    // async update(@Param('id', ParseIntPipe) id, @Body() { name, horas, faltas }) {
+    //     return this.materia.update(id, { name, horas, faltas })
+    // }
 
 
 } 
--- src/materia/materia.service.ts
+++ src/materia/materia.service.ts
@@ -7,24 +7,86 @@ export class MateriaService {
 
     constructor(private readonly prisma: PrismaService) { }
 
-    async readAll() {
-        return this.prisma.materias.findMany()
+    async readAll({ aluno_id }) {
+
+        const aluno = await this.prisma.alunos.findUnique({
+            where: {
+                id: Number(aluno_id)
+            }
+        })
+        if (!aluno) {
+            throw new Error(`Aluno com ID ${aluno_id} não encontrado.`);
+        }
+
+        return this.prisma.materias.findMany({
+            where: {
+                aluno_id: aluno.id
+            }
+        })
     }
 
-    async create({ name, horas, faltas }) {
-        return this.prisma.materias.create(
+    async createAluno({ name, password }) {
+        return this.prisma.alunos.create(
             {
-                data: { name, horas, faltas }
+                data: {
+                    nome: name,
+                    password: password
+                }
             }
         );
     }
 
-    async delete(id) {
-        return this.prisma.materias.delete({
+    async createMateira({ aluno_id, nome, horas, faltas }) {
+        const aluno = await this.prisma.alunos.findUnique({
             where: {
-                id
+                id: Number(aluno_id)
             }
         })
+        if (!aluno) {
+            throw new Error(`Aluno com ID ${aluno_id} não encontrado.`);
+        }
+
+        return await this.prisma.materias.create({
+            data: {
+                aluno_id: aluno.id,
+                nome: nome,
+                horas: horas,
+                faltas: faltas,
+            }
+        })
+
+
     }
+
+    // async delete(id) {
+    //     return this.prisma.aluno.delete({
+    //         where: {
+    //             id
+    //         }
+    //     })
+    // }
+
+    // async update(id, { name, horas, faltas }) {
+
+    //     const data: any = {}
+
+    //     if (name) {
+    //         data.name = name;
+    //     }
+    //     if (horas) {
+    //         data.horas = horas
+    //     }
+    //     if (faltas) {
+    //         data.faltas = faltas
+    //     }
+
+    //     return this.prisma.aluno.update({
+    //         data,
+    //         where: {
+    //             id
+    //         },
+
+    //     })
+    // }
 
 }
